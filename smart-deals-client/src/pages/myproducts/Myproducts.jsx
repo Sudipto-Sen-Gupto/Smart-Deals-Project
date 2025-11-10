e1f5c2e28822078ee8b602ce33baa47a9f3fe9ff
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-11-10 11:08:06 +0600

    enviroment set up and security

--- smart-deals-client/src/pages/myproducts/Myproducts.jsx
+++ smart-deals-client/src/pages/myproducts/Myproducts.jsx
@@ -3,17 +3,24 @@ import { AuthContext } from '../../component/Authprovider/Authprovider';
 
 const Myproducts = () => {
     const {user}=use(AuthContext);
+
+    console.log(user?.accessToken);
     const[myProducts,setMyproducts]=useState([]);
     useEffect(()=>{
         if(user?.email){
-            fetch(`http://localhost:3000/biduser?email=${user.email}`).then(res=>res.json()).then(data=>{console.log(data)
+            fetch(`http://localhost:3000/biduser?email=dip@gmail.com`,{
+              headers:{
+                authorization:`bearer ${user?.accessToken}`
+            }
+            }).then(res=>res.json()).then(data=>{console.log(data)
+                console.log(data);
                 setMyproducts(data)}
         )
         }
-    },[])
+    },[user])
     return (
         <div>
-            My product:{myProducts.length+1}
+            My product:{myProducts.length}
         </div>
     );
 };
